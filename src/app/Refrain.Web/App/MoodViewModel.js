051b5004e461c6f7bce47045e76bd0e8d545cb3b
Jacob Poul Richardt <spam@poulrichardt.dk>
2014-05-05 20:28:14 +0200

    Added tweets to mood.

--- src/app/Refrain.Web/App/MoodViewModel.js
+++ src/app/Refrain.Web/App/MoodViewModel.js
@@ -1,5 +1,6 @@
 ﻿var MoodViewModel = (function () {
     function MoodViewModel() {
+        this.SelectedTweets = ko.observableArray();
         this._moodData = {};
     }
     MoodViewModel.prototype.Initialize = function () {
@@ -14,6 +15,7 @@
 
     MoodViewModel.prototype.PortalReady = function () {
         RefrainPortal.TwitterMood.Get().WithCallback(this.TwitterMoodGetCompleted, this);
+        RefrainPortal.Tweet.Get().WithCallback(this.TweetGetCompleted, this);
     };
 
     MoodViewModel.prototype.TwitterMoodGetCompleted = function (response) {
@@ -31,6 +33,44 @@
         this._map.data.setStyle(function (f) {
             return _this.SetCountryStyle(f);
         });
+    };
+
+    MoodViewModel.prototype.TweetGetCompleted = function (response) {
+        if (response.Error != null) {
+            console.log("Failed to get Twitter mood: " + response.Error.Message);
+            return;
+        }
+
+        var groups = response.Body.Groups;
+        var randomGroupIndex = 0;
+        var randomTweetIndex = 0;
+        var group = null;
+        var tweet = null;
+
+        while (groups.length != 0 && this.SelectedTweets().length < 10) {
+            randomGroupIndex = Math.floor(Math.random() * (groups.length - 1));
+
+            group = groups[randomGroupIndex];
+
+            if (group.Results == null || group.Results.length == 0) {
+                group.splice(randomGroupIndex, 1);
+                continue;
+            }
+
+            randomTweetIndex = Math.floor(Math.random() * (group.Results.length - 1));
+
+            tweet = group.Results[randomTweetIndex];
+
+            if (tweet.EmbedCode) {
+                var code = decodeURIComponent(tweet.EmbedCode.replace(/\+/g, '%20'));
+                code = code.substring(code.indexOf("<blockquote"), code.indexOf("</blockquote>") + 12);
+                this.SelectedTweets.push(code);
+            }
+
+            group.Results.splice(randomTweetIndex, 1);
+        }
+
+        twttr.widgets.load();
     };
 
     MoodViewModel.prototype.Capitalize = function (country) {
--- src/app/Refrain.Web/App/MoodViewModel.ts
+++ src/app/Refrain.Web/App/MoodViewModel.ts
@@ -2,6 +2,8 @@
 
 class MoodViewModel implements IPageViewModel
 {
+	public SelectedTweets: KnockoutObservableArray<string> = ko.observableArray<string>();
+
 	private _map: google.maps.Map;
 	private _moodData: { [countryCode: string]: number } = {};
 
@@ -24,6 +26,7 @@ class MoodViewModel implements IPageViewModel
 	public PortalReady(): void
 	{
 		RefrainPortal.TwitterMood.Get().WithCallback(this.TwitterMoodGetCompleted, this);
+		RefrainPortal.Tweet.Get().WithCallback(this.TweetGetCompleted, this);
 	}
 
 	private TwitterMoodGetCompleted(response: CHAOS.Portal.Client.IPortalResponse<any>):void
@@ -40,6 +43,49 @@ class MoodViewModel implements IPageViewModel
 			this._moodData[this.Capitalize(<string>groups[i].Value)] = groups[i].Results[0].Valence;
 
 		(<any>this._map).data.setStyle((f:any) => this.SetCountryStyle(f));
+	}
+
+	private TweetGetCompleted(response: CHAOS.Portal.Client.IPortalResponse<any>): void
+	{
+		if (response.Error != null)
+		{
+			console.log("Failed to get Twitter mood: " + response.Error.Message);
+			return;
+		}
+
+		var groups = <any[]>(<any>response.Body).Groups;
+		var randomGroupIndex = 0;
+		var randomTweetIndex = 0;
+		var group: any = null;
+		var tweet:any = null;
+
+		while (groups.length != 0 && this.SelectedTweets().length < 10)
+		{
+			randomGroupIndex = Math.floor(Math.random() * (groups.length - 1));
+
+			group = groups[randomGroupIndex];
+
+			if (group.Results == null || group.Results.length == 0)
+			{
+				group.splice(randomGroupIndex, 1);
+				continue;
+			}
+
+			randomTweetIndex = Math.floor(Math.random() * (group.Results.length - 1));
+
+			tweet = group.Results[randomTweetIndex];
+
+			if (tweet.EmbedCode)
+			{
+				var code = decodeURIComponent(tweet.EmbedCode.replace(/\+/g, '%20'));
+				code = code.substring(code.indexOf("<blockquote"), code.indexOf("</blockquote>") + 12);
+				this.SelectedTweets.push(code);
+			}				
+
+			group.Results.splice(randomTweetIndex, 1);
+		}
+
+		twttr.widgets.load();
 	}
 
 	private Capitalize(country:string):string
