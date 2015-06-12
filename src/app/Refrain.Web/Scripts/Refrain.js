567b97f1b9f3b8eb1f03e291441ab5c0b46d638e
Jacob Poul Richardt <spam@poulrichardt.dk>
2015-06-12 23:21:43 +0200

    Updated to production experiments.

--- src/app/Refrain.Web/Scripts/Refrain.js
+++ src/app/Refrain.Web/Scripts/Refrain.js
@@ -878,24 +878,6 @@ var RefrainPortal;
     })();
     RefrainPortal.MetricRating = MetricRating;
 })(RefrainPortal || (RefrainPortal = {}));
-var SimpleSongViewModel = (function () {
-    function SimpleSongViewModel(song) {
-        this.Year = null;
-        this.YoutubeUri = null;
-        this.SpotifyId = null;
-        this.Id = song.Id;
-        this.Title = song.Text;
-        this.Artist = song.ArtistName;
-        this.CountryName = song.CountryName;
-        this.CountryCode = CountryInfo[song.CountryName] ? CountryInfo[song.CountryName] : null;
-        this.Year = song.ContestYear;
-        if (song.YoutubeUri)
-            this.YoutubeUri = song.YoutubeUri;
-        if (song.SpotifyId)
-            this.SpotifyId = song.SpotifyId;
-    }
-    return SimpleSongViewModel;
-})();
 var Song = (function () {
     function Song(song, selector) {
         this.Year = null;
@@ -975,4 +957,22 @@ var SongSimilarity = (function () {
         return false;
     };
     return SongSimilarity;
+})();
+var SimpleSongViewModel = (function () {
+    function SimpleSongViewModel(song) {
+        this.Year = null;
+        this.YoutubeUri = null;
+        this.SpotifyId = null;
+        this.Id = song.Id;
+        this.Title = song.Text;
+        this.Artist = song.ArtistName;
+        this.CountryName = song.CountryName;
+        this.CountryCode = CountryInfo[song.CountryName] ? CountryInfo[song.CountryName] : null;
+        this.Year = song.ContestYear;
+        if (song.YoutubeUri)
+            this.YoutubeUri = song.YoutubeUri;
+        if (song.SpotifyId)
+            this.SpotifyId = song.SpotifyId;
+    }
+    return SimpleSongViewModel;
 })();
